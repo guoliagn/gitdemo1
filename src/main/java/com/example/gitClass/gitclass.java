5073bb8d6fb2eb1cb8ca8d605dc442bd6335a5f6
zgl <1529572035@qq.com>
2022-05-30 15:36:38 +0800

    hotfix first commit

--- src/main/java/com/example/gitClass/gitclass.java
+++ src/main/java/com/example/gitClass/gitclass.java
@@ -4,5 +4,6 @@ public class gitclass {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("ji");
+        System.out.println("hello");
     }
 }
